e0d7f606ed1d4cac5f61f76070d429c111a452da
Rayhan Bhuiyan <cliprandom1@gmail.com>
2023-04-20 23:17:26 +0600

    Inear page design runing

--- assets/js/inre-page.js
+++ assets/js/inre-page.js
@@ -28,11 +28,39 @@
                     slidesPerView: 1,
                 },
                 992: {
-                    slidesPerView: 2,
+                    slidesPerView: 3,
                 },
             },
         });
-
+        
+                /*====== testimonial -slider ======*/
+        var testimonial_sider = new Swiper(".testimonial-slider", {
+            loop: true,
+            speed: 800,
+            spaceBetween: 30,
+            slidesPerView: 1,
+            watchSlidesVisibility: true,
+            watchSlidesProgress: true,
+            navigation: {
+                nextEl: "#Project-slider-control .navigation-control .next",
+                prevEl: "#Project-slider-control .navigation-control .prev",
+            },
+            pagination: {
+                el: "#Project-slider-control .pagination-control",
+                clickable: true,
+            },
+            breakpoints: {
+                575: {
+                    slidesPerView: 1,
+                },
+                768: {
+                    slidesPerView: 1,
+                },
+                992: {
+                    slidesPerView: 3,
+                },
+            },
+        });
 
         /*====== Members-slider -slider ======*/
         var members_slider = new Swiper(".members-slider", {
@@ -62,40 +90,35 @@
                 },
             },
         });
-
-
-
-                /*====== payment-logo-slider -slider ======*/
-                var logo_slider = new Swiper(".logo-slider", {
+                /*====== team_member-Slider ======*/
+                var team_member_Slider = new Swiper(".team-member-slider", {
                     loop: true,
                     speed: 800,
-                    spaceBetween: 20,
+                    spaceBetween: 35,
                     slidesPerView: 1,
                     watchSlidesVisibility: true,
                     watchSlidesProgress: true,
                     navigation: {
-                        nextEl: "#Project-slider-control .navigation-control .next",
-                        prevEl: "#Project-slider-control .navigation-control .prev",
+                        nextEl: "#team-member-slider-control .navigation-control .next",
+                        prevEl: "#team-member-slider-control .navigation-control .prev",
                     },
                     pagination: {
-                        el: "#Project-slider-control .pagination-control",
+                        el: "#team-member-slider-control .pagination-control",
                         clickable: true,
+                        type: 'bullets',
+                        renderBullet: function (i) {
+                            return `<span class="dot swiper-pagination-bullet" ><svg> <circle style="animation-duration: ` + 3000 / 1000 + `s;" cx="11" cy="11" r="10"></circle></svg></span>`;
+                        }
                     },
                     breakpoints: {
-                        320: {
+                        576: {
                             slidesPerView: 2,
                         },
-                        575: {
+                        992: {
                             slidesPerView: 3,
                         },
-                        768: {
-                            slidesPerView: 4,
-                        },
-                        992: {
-                            slidesPerView: 5,
-                        },
                         1200: {
-                            slidesPerView: 6,
+                            slidesPerView: 4,
                         },
                     },
                 });
@@ -103,26 +126,7 @@
 
 
 
-
-        $('.bar_group').each(function () {
-            $(this).appear(function () {
-                var value = $(this).find('.bar_group__bar').data('value');
-                $('.b_tooltip span').each(function () {
-                    var $this = $(this);
-                    if (!$this.data('animated')) {
-                        $this.data('animated', true).animateNumber({
-                            number: value
-                        }, 1500);
-                    }
-                });
-            });
-        });
-
-
-          
-
-
-
+        
     });
 
 })(jQuery);
