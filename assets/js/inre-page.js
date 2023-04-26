a1a6b5276209ab6caa16a55657c935c2341a21d3
Rayhan Bhuiyan <cliprandom1@gmail.com>
2023-04-26 18:53:23 +0600

    iner page almost complet

--- assets/js/inre-page.js
+++ assets/js/inre-page.js
@@ -123,7 +123,58 @@
                     },
                 });
 
+                                /*====== service-heade-slider ======*/
+                                var logo_slider = new Swiper(".logo-slider", {
+                                    loop: true,
+                                    speed: 800,
+                                    spaceBetween: 10,
+                                    slidesPerView: 1,
+                                    watchSlidesVisibility: true,
+                                    watchSlidesProgress: true,
+                                    navigation: {
+                                        nextEl: "#Project-slider-control .navigation-control .next",
+                                        prevEl: "#Project-slider-control .navigation-control .prev",
+                                    },
+                                    pagination: {
+                                        el: "#Project-slider-control .pagination-control",
+                                        clickable: true,
+                                    },
+                                    breakpoints: {
+                                        320: {
+                                            slidesPerView: 2,
+                                        },
+                                        575: {
+                                            slidesPerView: 3,
+                                        },
+                                        768: {
+                                            slidesPerView: 4,
+                                        },
+                                        992: {
+                                            slidesPerView: 5,
+                                        },
+                                    },
+                                });
+                
 
+
+                                        /*====== service-2-testimonial-slider ======*/
+        var testimonial_sider = new Swiper(".service-2-testimonial-slider", {
+            loop: true,
+            speed: 800,
+            spaceBetween: 30,
+            slidesPerView: 1,
+            watchSlidesVisibility: true,
+            watchSlidesProgress: true,
+            navigation: {
+                nextEl: ".service-2-testimonial-slider .navigation-control .next",
+                prevEl: ".service-2-testimonial-slider .navigation-control .prev",
+            },
+            breakpoints: {
+                575: {
+                    slidesPerView: 1,
+                },
+            },
+        });
 
 
         
