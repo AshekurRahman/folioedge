37ec3ba996bd2a508e68efaaba8656e069775a45
Rayhan Bhuiyan <cliprandom1@gmail.com>
2023-04-30 10:57:07 +0600

    inear page al most compleat responesive runing

--- assets/js/inre-page.js
+++ assets/js/inre-page.js
@@ -32,8 +32,8 @@
                 },
             },
         });
-        
-                /*====== testimonial -slider ======*/
+
+        /*====== testimonial -slider ======*/
         var testimonial_sider = new Swiper(".testimonial-slider", {
             loop: true,
             speed: 800,
@@ -90,74 +90,74 @@
                 },
             },
         });
-                /*====== team_member-Slider ======*/
-                var team_member_Slider = new Swiper(".team-member-slider", {
-                    loop: true,
-                    speed: 800,
-                    spaceBetween: 35,
-                    slidesPerView: 1,
-                    watchSlidesVisibility: true,
-                    watchSlidesProgress: true,
-                    navigation: {
-                        nextEl: "#team-member-slider-control .navigation-control .next",
-                        prevEl: "#team-member-slider-control .navigation-control .prev",
-                    },
-                    pagination: {
-                        el: "#team-member-slider-control .pagination-control",
-                        clickable: true,
-                        type: 'bullets',
-                        renderBullet: function (i) {
-                            return `<span class="dot swiper-pagination-bullet" ><svg> <circle style="animation-duration: ` + 3000 / 1000 + `s;" cx="11" cy="11" r="10"></circle></svg></span>`;
-                        }
-                    },
-                    breakpoints: {
-                        576: {
-                            slidesPerView: 2,
-                        },
-                        992: {
-                            slidesPerView: 3,
-                        },
-                        1200: {
-                            slidesPerView: 4,
-                        },
-                    },
-                });
+        /*====== team_member-Slider ======*/
+        var team_member_Slider = new Swiper(".team-member-slider", {
+            loop: true,
+            speed: 800,
+            spaceBetween: 35,
+            slidesPerView: 1,
+            watchSlidesVisibility: true,
+            watchSlidesProgress: true,
+            navigation: {
+                nextEl: "#team-member-slider-control .navigation-control .next",
+                prevEl: "#team-member-slider-control .navigation-control .prev",
+            },
+            pagination: {
+                el: "#team-member-slider-control .pagination-control",
+                clickable: true,
+                type: 'bullets',
+                renderBullet: function (i) {
+                    return `<span class="dot swiper-pagination-bullet" ><svg> <circle style="animation-duration: ` + 3000 / 1000 + `s;" cx="11" cy="11" r="10"></circle></svg></span>`;
+                }
+            },
+            breakpoints: {
+                576: {
+                    slidesPerView: 2,
+                },
+                992: {
+                    slidesPerView: 3,
+                },
+                1200: {
+                    slidesPerView: 4,
+                },
+            },
+        });
 
-                                /*====== service-heade-slider ======*/
-                                var logo_slider = new Swiper(".logo-slider", {
-                                    loop: true,
-                                    speed: 800,
-                                    spaceBetween: 10,
-                                    slidesPerView: 1,
-                                    watchSlidesVisibility: true,
-                                    watchSlidesProgress: true,
-                                    navigation: {
-                                        nextEl: "#Project-slider-control .navigation-control .next",
-                                        prevEl: "#Project-slider-control .navigation-control .prev",
-                                    },
-                                    pagination: {
-                                        el: "#Project-slider-control .pagination-control",
-                                        clickable: true,
-                                    },
-                                    breakpoints: {
-                                        320: {
-                                            slidesPerView: 2,
-                                        },
-                                        575: {
-                                            slidesPerView: 3,
-                                        },
-                                        768: {
-                                            slidesPerView: 4,
-                                        },
-                                        992: {
-                                            slidesPerView: 5,
-                                        },
-                                    },
-                                });
-                
+        /*====== service-heade-slider ======*/
+        var logo_slider = new Swiper(".logo-slider", {
+            loop: true,
+            speed: 800,
+            spaceBetween: 10,
+            slidesPerView: 1,
+            watchSlidesVisibility: true,
+            watchSlidesProgress: true,
+            navigation: {
+                nextEl: "#Project-slider-control .navigation-control .next",
+                prevEl: "#Project-slider-control .navigation-control .prev",
+            },
+            pagination: {
+                el: "#Project-slider-control .pagination-control",
+                clickable: true,
+            },
+            breakpoints: {
+                320: {
+                    slidesPerView: 2,
+                },
+                575: {
+                    slidesPerView: 3,
+                },
+                768: {
+                    slidesPerView: 4,
+                },
+                992: {
+                    slidesPerView: 5,
+                },
+            },
+        });
 
 
-                                        /*====== service-2-testimonial-slider ======*/
+
+        /*====== service-2-testimonial-slider ======*/
         var testimonial_sider = new Swiper(".service-2-testimonial-slider", {
             loop: true,
             speed: 800,
@@ -177,7 +177,7 @@
         });
 
 
-        
+
     });
 
 })(jQuery);
